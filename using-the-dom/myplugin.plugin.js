f523512fb14e8be0aa1e3b426e2a4d8441d72b1e
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-18 18:24:06 -0600

    Using the dom step 1

---
+++ using-the-dom/myplugin.plugin.js
@@ -0,0 +1,19 @@
+/**
+ * @name Using The DOM
+ * @author ArkhamCookie
+ * @description The plugin that interacts with the DOM
+ * @version 0.0.1
+ */
+
+module.exports = class BasicPlugin {
+	start() {
+		const myButton = document.createElement('button')
+		myButton.textContent = 'Click me!'
+		myButton.addEventListener('click', () => { window.alert('Hello World!') })
+
+		const root = document.getElementById('app-mount')
+		root.append(myButton)
+	}
+
+	stop() {}
+}
