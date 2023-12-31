330a70846d7569e6e2811020b731486b8f0dd323
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-31 11:20:39 -0600

    Append to top of server list
    Instead of root

--- using-the-dom/myplugin.plugin.js
+++ using-the-dom/myplugin.plugin.js
@@ -11,8 +11,8 @@ module.exports = class BasicPlugin {
 		myButton.textContent = 'Click me!'
 		myButton.addEventListener('click', () => { window.alert('Hello World!') })
 
-		const root = document.getElementById('app-mount')
-		root.append(myButton)
+		const serverList = document.querySelector('#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > nav > ul > div.scroller__3d071.none__51a8f.scrollerBase_dc3aa9 > div:nth-child(3)')
+		serverList.append(myButton)
 	}
 
 	stop() {}
