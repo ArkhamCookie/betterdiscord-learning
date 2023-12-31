2492e981e63b5ca0adb2e054299776df0426425e
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-31 11:36:17 -0600

    Fixed typo in rm section

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,10 @@
 case "$1" in
 'rm')
 	if [ -n "$2" ]; then
-		rm "$HOME"/.config/BetterDiscord/plugin/"$2"
+		rm "$HOME"/.config/BetterDiscord/plugins/"$2"
 		exit
 	fi
-	rm "$HOME"/.config/BetterDiscord/plugin/myplugin.plugin.js
+	rm "$HOME"/.config/BetterDiscord/plugins/myplugin.plugin.js
 	exit
 ;;
 '')
